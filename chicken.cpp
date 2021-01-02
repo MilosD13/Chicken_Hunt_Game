200db5ba7e76842f76bc5cf4ce1f9d8bb6943c46
Milos Dokic <milosdokic13@gmail.com>
2021-01-02 18:12:43 +1100

    test

--- chicken.cpp
+++ chicken.cpp
@@ -28,29 +28,29 @@ chicken_data new_chicken()
 {
     chicken_data result;
     // Defines random bitmap
-    result.kind = static_cast<chicken_kind>(rnd(0,3));
+    result.kind = static_cast<chicken_kind>(rnd(0, 3));
 
     // Create random sprite and random speed for it
     if (result.kind == 2)
     {
         result.chicken_sprite = create_sprite("ani_chicken", "chicken");
-        sprite_start_animation(result.chicken_sprite,0);
-        sprite_set_dx(result.chicken_sprite, rnd()*4 + 2);
+        sprite_start_animation(result.chicken_sprite, 0);
+        sprite_set_dx(result.chicken_sprite, rnd() * 4 + 2);
     }
     else if (result.kind == 0)
     {
         result.chicken_sprite = create_sprite("ani_chicken_s", "chicken_s");
-        sprite_start_animation(result.chicken_sprite,0);
-        sprite_set_dx(result.chicken_sprite, rnd()*4 + 2);
+        sprite_start_animation(result.chicken_sprite, 0);
+        sprite_set_dx(result.chicken_sprite, rnd() * 4 + 2);
     }
     else
     {
-    result.chicken_sprite = create_sprite("ani_chicken_l", "chicken_l");
-    sprite_start_animation(result.chicken_sprite,0);
-    sprite_set_dx(result.chicken_sprite, rnd()*4 + 2);
+        result.chicken_sprite = create_sprite("ani_chicken_l", "chicken_l");
+        sprite_start_animation(result.chicken_sprite, 0);
+        sprite_set_dx(result.chicken_sprite, rnd() * 4 + 2);
     }
-    float random_x = rnd(-20,-10);
-    float random_y = rand()%500;
+    float random_x = rnd(-20, -10);
+    float random_y = rand() % 500;
     sprite_set_x(result.chicken_sprite, random_x);
     sprite_set_y(result.chicken_sprite, random_y);
 
@@ -70,31 +70,31 @@ void update_chicken(chicken_data &chicken_to_update)
     update_sprite(chicken_to_update.chicken_sprite);
 }
 
-//applies power to the user when colided
+//applies power to the user when colided with user
 void apply_chicken_collision(chicken_data &chickens, player_data &player)
 {
-    switch(chickens.kind)
+    switch (chickens.kind)
     {
-        case S_CHICKEN:
-            player.score += S_CHICKEN_ADD_SCORE;
-            break;
-        case L_CHICKEN:
-            player.score += L_CHICKEN_ADD_SCORE;
-            break;
-        case M_CHICKEN:
-            player.score += M_CHICKEN_ADD_SCORE;
-            break;
+    case S_CHICKEN:
+        player.score += S_CHICKEN_ADD_SCORE;
+        break;
+    case L_CHICKEN:
+        player.score += L_CHICKEN_ADD_SCORE;
+        break;
+    case M_CHICKEN:
+        player.score += M_CHICKEN_ADD_SCORE;
+        break;
     }
 }
 
 /* checks for colision between player and chicken */
 void check_chicken_collision(player_data &player, vector<chicken_data> &chickens)
 {
-  for(int i = 0; i < chickens.size(); i++)
+    for (int i = 0; i < chickens.size(); i++)
     {
-        if(sprite_collision(chickens[i].chicken_sprite, player.player_sprite) && mouse_clicked(LEFT_BUTTON) && player.bullets > 0 )
+        if (sprite_collision(chickens[i].chicken_sprite, player.player_sprite) && mouse_clicked(LEFT_BUTTON) && player.bullets > 0)
         {
-            sprite_set_dx(chickens[i].chicken_sprite,0);
+            sprite_set_dx(chickens[i].chicken_sprite, 0);
             apply_chicken_collision(chickens[i], player);
             chickens.erase(chickens.begin() + i);
         }
